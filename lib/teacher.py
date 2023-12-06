d192390019be8eca2416122110de63ec706b9337
Pat0h <patrickngatia14@gmail.com>
2023-12-06 18:16:57 +0300

    complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,14 @@ from user import User
 
 import random
 
+# teacher.py
+
+from user import User
+
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
